83d6661e45c0a548cb801ad5cbe0d87091e46d16
pleasurazy <pleasurazy@gmail.com>
2015-09-18 15:23:34 +0800

    jsdoc

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import isUrl from 'is-url'
 import request from 'request-promise'
 import bloggers from './bloggerAPI'
 
+/**
+@name Article
+@interface
+@prop {string[]} address
+@prop {string} body - Content of article
+@prop {string} cover - Generally first element of Article.images
+@prop {string[]} images - Contain URL format in the array.
+@prop {string} published - ISO8601 format
+@prop {string} title
+@prop {string} url
+*/
+
+/**
+@name findAll
+@param {object} options
+@param {boolean} [options.fetchAll=false] - If you want to retrieve all the articles form google.
+@param {string} options.url - Url of Blogspot.
+@param {string} options.key - Server-side API access token from Google Blogger API.
+@returns {Article[]}
+*/
 async function findAll(opts = {}) {
 
   opts = _.defaultsDeep({}, opts, {
